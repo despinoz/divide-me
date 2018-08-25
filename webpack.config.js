2500f20ae1093579e78b346a4482a7d58b0cb120
Diego Espinoza <diespinozas@gmail.com>
2018-08-25 11:23:49 -0700

    base-setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  }
+}
