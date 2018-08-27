8dc07e0f4bf620628ef8f00fafff01d281cffb36
Diego Espinoza <diespinozas@gmail.com>
2018-08-27 10:42:59 -0700

    extend base schema creation for database

--- db/index.js
+++ db/index.js
@@ -1,3 +1,15 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+mongoose.connect('mongodb://localhost/divide-me', { useNewUrlParser: true });
+
+const BillSchema = new Schema({
+  owner: String,
+  users: {
+    user: String,
+  },
+  name: String,
+});
+const Bill = mongoose.model('Bill', BillSchema);
 
 mongoose.connect('mongodb://localhost/divide-me');
