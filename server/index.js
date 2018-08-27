5244eb6e3de9ac858644a8b0d33a127dc38fe1cf
Diego Espinoza <diespinozas@gmail.com>
2018-08-27 10:44:59 -0700

    added basic routing

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const Log = require('log');
+const db = require('../db');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const log = new Log('info');
 
+app.get('/bill', (req, res) => {
+  // log.info(create);
+  res.send('GET');
+});
+
+app.post('/bill', (req, res) => {
+  db.create((data) => {
+    // log.info(data.id);
+    res.send(data.id);
+  });
+});
 
 app.use(express.static('client/dist'));
 
