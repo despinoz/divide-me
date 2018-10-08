2619552b72916d1bc83d898a9c62d8401aee70a9
Diego Espinoza <diespinozas@gmail.com>
2018-10-07 23:35:49 -0700

    added database handling file

--- db/index.js
+++ db/index.js
@@ -7,23 +7,58 @@ const { Schema } = mongoose;
 
 mongoose.connect('mongodb://localhost/divide-me', { useNewUrlParser: true });
 
+const ItemSchema = new Schema({
+  name: String,
+  quantity: Number,
+  unitPrice: Number,
+});
+
 const BillSchema = new Schema({
   owner: String,
-  users: {
-    user: String,
-  },
+  items: [ItemSchema],
+  users: [String],
   name: String,
 });
+
+// BillSchema.methods.speak = () => {
+//   log.info(this)
+//   const greeting = this.name
+//     ? `Meow name is ${this.name}`
+//     : "I don't have a name";
+//   log.info(greeting);
+// };
 
 const Bill = mongoose.model('Bill', BillSchema);
 
 const create = (callback) => {
-  Bill.create({ }, (err, result) => {
+  Bill.create({ }, (err, res) => {
     if (err) log.error(err);
-    callback(result);
+    callback(res);
+  });
+};
+
+const update = (id, item, callback) => {
+  // console.log(item)
+  // const newItem = new ItemSchema(item);
+  Bill.updateOne({ _id: id }, { $push: { items: item } }, (err, res) => {
+    if (err) log.error(err);
+    callback(res);
+  });
+};
+
+const find = (id, callback) => {
+  Bill.findOne({ _id: id }, (err, res) => {
+    if (err) log.error(err);
+    callback(res);
   });
 };
 
 module.exports = {
   create,
+  update,
+  find,
 };
+
+// Bill.findById('5b83a66c19eafe19bea65bed', (err, adventure) => {
+//   if (err) log.error(err);
+// });
